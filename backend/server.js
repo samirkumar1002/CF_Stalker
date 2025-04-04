5423a94a5d9b0fd2bbbe9f2c83e93905c767f174
Ash590034 <ashutoshpatel9443@gmail.com>
2025-04-05 02:05:49 +0530

    Changed cors to allow All

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all routes with specific configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Vite's default ports
+  origin: ['*'], // Vite's default ports
   methods: ['GET', 'POST'],
   credentials: true
 }));
